03a560f0cd24ba6595673dc91dc0ec1260d94a25
Yuxis445 <luffy2033@outlook.com>
2021-12-02 10:59:20 -0300

    Spring post, put, delete

--- blogpessoal/src/main/java/org/generation/blogpessoal/controller/PostagemController.java
+++ blogpessoal/src/main/java/org/generation/blogpessoal/controller/PostagemController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.generation.blogpessoal.model.Postagem;
 import org.generation.blogpessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +44,26 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
+
+    // inserir dados no banco de dados
+    @PostMapping
+    public ResponseEntity<Postagem> post(@RequestBody Postagem postagem)
+    {
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+    }
+
+    // atualizar um dado ja existente no banco de dados
+    @PutMapping
+    public ResponseEntity<Postagem> put(@RequestBody Postagem postagem)
+    {
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+    }
+
+    //deletar dados do banco de dados.
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id)
+    {
+        repository.deleteById(id);
+    }
 
 }
